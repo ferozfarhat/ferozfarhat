6a9eadc85b12f2170c950ceff16d8be68d6585f8
Feroz <79737085+ferozfarhat@users.noreply.github.com>
2024-09-23 23:04:08 +0530

    done

--- script/stop_container.sh
+++ script/stop_container.sh
@@ -1,23 +1,6 @@
 #!/bin/bash
+set -e
 
-# Define variables
-CONTAINER_NAME="your_container_name"
-
-# Check if the container is running
-if [ "$(docker ps -q -f name=${CONTAINER_NAME})" ]; then
-    echo "Stopping the container ${CONTAINER_NAME}..."
-    docker stop ${CONTAINER_NAME}
-else
-    echo "Container ${CONTAINER_NAME} is not running."
-fi
-
-# Check if the container exists
-if [ "$(docker ps -aq -f name=${CONTAINER_NAME})" ]; then
-    echo "Removing the container ${CONTAINER_NAME}..."
-    docker rm ${CONTAINER_NAME}
-else
-    echo "Container ${CONTAINER_NAME} does not exist."
-fi
-
-echo "Container ${CONTAINER_NAME} has been stopped and removed."
-
+#Stop the container (if any)
+containerid='docker ps | awk -F "" '{print $1}'
+docker rm - f dockerid
