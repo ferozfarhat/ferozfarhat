22674063b5291a51b7f273385d3ee84f3f18513c
Feroz <79737085+ferozfarhat@users.noreply.github.com>
2024-09-23 23:31:21 +0530

    path changed

--- start_container.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# pull the docker image from docker hub
-docker pull docker pull feroztt2/ferozfarhat
-
-# Run the docker image as a container
-docker run -d -p 5000:5000 feroztt2/ferozfarhat
--- stop_container.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-#Stop the container (if any)
-containerid='docker ps | awk -F "" '{print $1}'
-docker rm - f dockerid
